d7a75281ad0fde9e6dc5f22499a0e02f30ecb4df
Maximilian Li <maximilian.xiling.li@gmail.com>
2017-06-13 22:43:36 +0200

    combat groundwork todo: combat proficiency labeling

--- app/editor/js/combat.js
+++ app/editor/js/combat.js
@@ -1,12 +1,19 @@
 var t = $('#tb_combat')[0];
 var r = t.rows;
 
-for (var i = 1; i < r.length; i++){
-    var c = r[i].cells;
 
-    for (var j = 0; j < c.length; j++){
-        var txt = c[j].textContent;
+function saveCombat() {
 
-        if (txt != '') console.log(txt);
+    for (var i = 1; i < r.length; i++) {
+        var c = r[i].cells;
+
+        for (var j = 0; j < c.length; j++) {
+            var txt = c[j].textContent;
+
+            if (txt != '')
+                character["cb_" + i + "_" + j] = txt
+        }
     }
+
+    console.log(character)
 }
